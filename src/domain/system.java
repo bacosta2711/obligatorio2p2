b3b6f6d25e5d0bebbd8557c3fa88ad4768257fe3
Bruno Acosta <94932425+bacosta2711@users.noreply.github.com>
2023-10-19 21:14:16 -0300

    Agrego un poco mas de cosas de la clase sistema

---
+++ src/domain/system.java
@@ -0,0 +1,32 @@
+
+package domain;
+
+import java.util.ArrayList;
+
+public class system {
+    private ArrayList<Interview> interviews;
+    private ArrayList<Topic> topics;
+    private ArrayList<Position> positions;
+    private ArrayList<Postulant> postulants;
+    private ArrayList<Interviewer> interviewers;
+
+    public ArrayList<Interview> getInterviews() {
+        return interviews;
+    }
+
+    public ArrayList<Topic> getTopics() {
+        return topics;
+    }
+
+    public ArrayList<Position> getPositions() {
+        return positions;
+    }
+
+    public ArrayList<Postulant> getPostulants() {
+        return postulants;
+    }
+
+    public ArrayList<Interviewer> getInterviewers() {
+        return interviewers;
+    }
+}
