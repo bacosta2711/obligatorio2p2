e8dcd6bf5ffdfb6650658ce3e79994ad9b75e08d
Bruno Acosta <94932425+bacosta2711@users.noreply.github.com>
2023-10-22 22:30:13 -0300

    Va arreglo

--- src/interfaces/HomePage.java
+++ src/interfaces/HomePage.java
@@ -40,10 +40,8 @@ public class HomePage extends javax.swing.JFrame {
         addPostulant = new javax.swing.JMenuItem();
         removePostulant = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
-
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
-
 
         jMenu3.setText("jMenu3");
 
@@ -53,7 +51,7 @@ public class HomePage extends javax.swing.JFrame {
 
         jLabel1.setText("Bienvenido a Entrevistas!");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(120, 110, 170, 16);
+        jLabel1.setBounds(120, 110, 170, 17);
 
         jMenu1.setText("Postulantes");
 
@@ -87,41 +85,8 @@ public class HomePage extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu2);
 
-        addInterviewer.setText("Evaluador");
-
-        jMenuItem1.setText("Agregar evaluador");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
-            }
-        });
-        addInterviewer.add(jMenuItem1);
-
-        jMenuBar1.add(addInterviewer);
-
-        jMenu5.setText("Entrevista");
-
-        jMenuItem3.setText("Agregar entrevista");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
-            }
-        });
-        jMenu5.add(jMenuItem3);
-
-        jMenuBar1.add(jMenu5);
-
-        jMenu6.setText("Posición");
-
-        jMenuItem2.setText("Agregar posición");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu6.add(jMenuItem2);
-
-        jMenuBar1.add(jMenu6);
+        jMenu4.setText("Evaluador");
+        jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
 
@@ -139,7 +104,6 @@ public class HomePage extends javax.swing.JFrame {
     }//GEN-LAST:event_removePostulantActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-
         this.setVisible(false);
         AddTopic window = new AddTopic(this.getSystem());
         window.setVisible(true);
@@ -149,79 +113,18 @@ public class HomePage extends javax.swing.JFrame {
 
     public SystemClass getSystem() {
         return system;
-
-        AddInterviewer interviewer = new AddInterviewer();
-        interviewer.setVisible(true);
-        
-// TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
-
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        AddInterview interview = new AddInterview();
-        interview.setVisible(true);
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
-
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        AddPosition position = new AddPosition();
-        position.setVisible(true);
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HomePage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HomePage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HomePage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HomePage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new HomePage().setVisible(true);
-                
-                
-            }
-        });
-        
-        
-        SystemClass system = new SystemClass();
-
     }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu addInterviewer;
     private javax.swing.JMenuItem addPostulant;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenu jMenu5;
-    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-
     private javax.swing.JMenuItem removePostulant;
     // End of variables declaration//GEN-END:variables
 }
